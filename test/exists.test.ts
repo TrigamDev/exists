b06550232e5ce5444b85c23994353286050331a6
TrigamDev <trigambusiness@gmail.com>
2026-01-26 17:23:54 -0500

    refactor: lint

--- test/exists.test.ts
+++ test/exists.test.ts
@@ -1,39 +1,42 @@
-import { expect, test, describe } from "bun:test"
+import {
+	describe,
+	expect, test
+} from "bun:test"
+
 import { exists } from "@exists/exists"
 
 describe( "exists( undefined )", () => {
 	test( "undefined doesn't exist by default", () => {
 		expect(
-			exists( undefined )
+			exists( undefined ) // eslint-disable-line no-undefined
 		).toBe( false )
-	})
+	} )
 
 	test( "undefined doesn't exist when nonExistent.undefined = true", () => {
 		expect(
 			exists(
-				undefined,
+				undefined, // eslint-disable-line no-undefined
 				{ nonExistent: { undefined: true } }
 			)
 		).toBe( false )
-	})
+	} )
 
 	test( "undefined exists when nonExistent.undefined = false", () => {
 		expect(
 			exists(
-				undefined,
+				undefined, // eslint-disable-line no-undefined
 				{ nonExistent: { undefined: false } }
 			)
 		).toBe( true )
-	})
-
-})
+	} )
+} )
 
 describe( "exists( null )", () => {
 	test( "null doesn't exist by default", () => {
 		expect(
 			exists( null )
 		).toBe( false )
-	})
+	} )
 
 	test( "null doesn't exist when nonExistent.null = true", () => {
 		expect(
@@ -42,7 +45,7 @@ describe( "exists( null )", () => {
 				{ nonExistent: { null: true } }
 			)
 		).toBe( false )
-	})
+	} )
 
 	test( "null exists when nonExistent.null = false", () => {
 		expect(
@@ -51,15 +54,15 @@ describe( "exists( null )", () => {
 				{ nonExistent: { null: false } }
 			)
 		).toBe( true )
-	})
-})
+	} )
+} )
 
 describe( "exists( false )", () => {
 	test( "false exists by default", () => {
 		expect(
 			exists( false )
 		).toBe( true )
-	})
+	} )
 
 	test( "false doesn't exist when nonExistent.false = true", () => {
 		expect(
@@ -68,7 +71,7 @@ describe( "exists( false )", () => {
 				{ nonExistent: { false: true } }
 			)
 		).toBe( false )
-	})
+	} )
 
 	test( "false exists when nonExistent.false = false", () => {
 		expect(
@@ -77,35 +80,50 @@ describe( "exists( false )", () => {
 				{ nonExistent: { false: false } }
 			)
 		).toBe( true )
-	})
+	} )
 
 	// Ensure falsy values aren't caught by nonExistent.false = true
 	test( "null exists when nonExistent.false = true", () => {
 		expect(
 			exists(
 				null,
-				{ nonExistent: { false: true, null: false } }
+				{
+					nonExistent: {
+						false: true,
+						null: false
+					}
+				}
 			)
 		).toBe( true )
-	})
+	} )
 
 	test( "undefined exists when nonExistent.false = true", () => {
 		expect(
 			exists(
-				undefined,
-				{ nonExistent: { false: true, undefined: false } }
+				undefined, // eslint-disable-line no-undefined
+				{
+					nonExistent: {
+						false: true,
+						undefined: false
+					}
+				}
 			)
 		).toBe( true )
-	})
+	} )
 
 	test( "NaN exists when nonExistent.false = true", () => {
 		expect(
 			exists(
 				NaN,
-				{ nonExistent: { false: true, nan: false } }
+				{
+					nonExistent: {
+						false: true,
+						nan: false
+					}
+				}
 			)
 		).toBe( true )
-	})
+	} )
 
 	test( "0 exists when nonExistent.false = true", () => {
 		expect(
@@ -114,7 +132,7 @@ describe( "exists( false )", () => {
 				{ nonExistent: { false: true } }
 			)
 		).toBe( true )
-	})
+	} )
 
 	test( "-0 exists when nonExistent.false = true", () => {
 		expect(
@@ -123,33 +141,38 @@ describe( "exists( false )", () => {
 				{ nonExistent: { false: true } }
 			)
 		).toBe( true )
-	})
+	} )
 
 	test( "0n exists when nonExistent.false = true", () => {
 		expect(
 			exists(
-				0n,
+				0n, // eslint-disable-line no-magic-numbers
 				{ nonExistent: { false: true } }
 			)
 		).toBe( true )
-	})
+	} )
 
-	test( `"" exists when nonExistent.false = true`, () => {
+	test( "\"\" exists when nonExistent.false = true", () => {
 		expect(
 			exists(
 				"",
-				{ nonExistent: { false: true, emptyStrings: false } }
+				{
+					nonExistent: {
+						false: true,
+						emptyStrings: false
+					}
+				}
 			)
 		).toBe( true )
-	})
-})
+	} )
+} )
 
 describe( "exists( nan )", () => {
 	test( "NaN exists by default", () => {
 		expect(
 			exists( NaN )
 		).toBe( true )
-	})
+	} )
 
 	test( "NaN doesn't exist when nonExistent.nan = true", () => {
 		expect(
@@ -158,7 +181,7 @@ describe( "exists( nan )", () => {
 				{ nonExistent: { nan: true } }
 			)
 		).toBe( false )
-	})
+	} )
 
 	test( "NaN exists when nonExistent.nan = false", () => {
 		expect(
@@ -167,41 +190,41 @@ describe( "exists( nan )", () => {
 				{ nonExistent: { nan: false } }
 			)
 		).toBe( true )
-	})
-})
+	} )
+} )
 
 describe( "exists( empty string )", () => {
-	test( `"" exists by default`, () => {
+	test( "\"\" exists by default", () => {
 		expect(
 			exists( "" )
 		).toBe( true )
-	})
+	} )
 
-	test( `"" doesn't exist when nonExistent.emptyStrings = true`, () => {
+	test( "\"\" doesn't exist when nonExistent.emptyStrings = true", () => {
 		expect(
 			exists(
 				"",
 				{ nonExistent: { emptyStrings: true } }
 			)
 		).toBe( false )
-	})
+	} )
 
-	test( `"" exists when nonExistent.emptyStrings = false`, () => {
+	test( "\"\" exists when nonExistent.emptyStrings = false", () => {
 		expect(
 			exists(
 				"",
 				{ nonExistent: { emptyStrings: false } }
 			)
 		).toBe( true )
-	})
-})
+	} )
+} )
 
 describe( "exists( empty array )", () => {
 	test( "[] exists by default", () => {
 		expect(
 			exists( [] )
 		).toBe( true )
-	})
+	} )
 
 	test( "[] doesn't exist when nonExistent.emptyArrays = true", () => {
 		expect(
@@ -210,7 +233,7 @@ describe( "exists( empty array )", () => {
 				{ nonExistent: { emptyArrays: true } }
 			)
 		).toBe( false )
-	})
+	} )
 
 	test( "[] exists when nonExistent.emptyArrays = false", () => {
 		expect(
@@ -219,15 +242,15 @@ describe( "exists( empty array )", () => {
 				{ nonExistent: { emptyArrays: false } }
 			)
 		).toBe( true )
-	})
-})
+	} )
+} )
 
 describe( "exists( empty object )", () => {
 	test( "{} exists by default", () => {
 		expect(
 			exists( {} )
 		).toBe( true )
-	})
+	} )
 
 	test( "{} doesn't exist when nonExistent.emptyObjects = true", () => {
 		expect(
@@ -236,7 +259,7 @@ describe( "exists( empty object )", () => {
 				{ nonExistent: { emptyObjects: true } }
 			)
 		).toBe( false )
-	})
+	} )
 
 	test( "{} exists when nonExistent.emptyObjects = false", () => {
 		expect(
@@ -245,5 +268,5 @@ describe( "exists( empty object )", () => {
 				{ nonExistent: { emptyObjects: false } }
 			)
 		).toBe( true )
-	})
-})
+	} )
+} )
