ce2c0734fa8275bb9b0c2589c8f6e7a7122232d2
TrigamDev <trigambusiness@gmail.com>
2026-02-04 21:23:13 -0500

    ops: update tsup config

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,8 +1,52 @@
-import { defineConfig } from "tsup"
+import {
+	defineConfig, type Options
+} from "tsup"
 
-export default defineConfig( {
-	entry: [ "./src/index.ts" ],
-	format: [ "esm", "cjs" ],
-	dts: true,
-	clean: true
-} )
+// eslint-disable-next-line complexity
+export function createTsupConfig ( {
+	entry = [ "src/index.ts" ],
+	external = [],
+	noExternal = [],
+	platform = "node",
+	format = [ "esm", "cjs" ],
+	target = "esnext",
+	skipNodeModulesBundle = true,
+	clean = true,
+	shims = format.includes( "cjs" ),
+	cjsInterop = format.includes( "cjs" ),
+	minify = false,
+	terserOptions = {
+		mangle: false,
+		keep_classnames: true,
+		keep_fnames: true
+	},
+	splitting = false,
+	keepNames = true,
+	dts = true,
+	sourcemap = true,
+	esbuildPlugins = [],
+	treeshake = false,
+	outDir = "dist"
+}: Options = {} ) {
+	return defineConfig( {
+		entry,
+		external,
+		noExternal,
+		platform,
+		format,
+		skipNodeModulesBundle,
+		target,
+		clean,
+		shims,
+		cjsInterop,
+		minify,
+		terserOptions,
+		splitting,
+		keepNames,
+		dts,
+		sourcemap,
+		esbuildPlugins,
+		treeshake,
+		outDir
+	} )
+}
